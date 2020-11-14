e2e2f44a32d8642bfad7f33e84a6d11ddd339416
SHI ZHEN <shi.zhen@vincit.fi>
2020-11-14 20:15:24 +0200

    feat(9.19): Entries added

--- patientor/types.ts
+++ patientor/types.ts
@@ -8,9 +8,47 @@ export enum Gender {
    Male = "male"
 }
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
-export interface Entry {
+
+interface BaseEntry {
+   id: string;
+   description: string;
+   date: string;
+   specialist: string;
+   diagnosisCodes?: Array<Diagnoses['code']>;
 }
 
+export enum HealthCheckRating {
+   "Healthy" = 0,
+   "LowRisk" = 1,
+   "HighRisk" = 2,
+   "CriticalRisk" = 3
+}
+
+interface HealthCheckEntry extends BaseEntry {
+   type: "HealthCheck";
+   healthCheckRating: HealthCheckRating;
+}
+
+interface HospitalEntry extends BaseEntry {
+   type: "Hospital";
+   discharge: {
+      date: string,
+      criteria: string
+   }
+}
+
+interface OccupationalHealthcareEntry extends BaseEntry {
+   type: "OccupationalHealthcare";
+   employerName: string,
+   sickLeave?: {
+      startDate: string,
+      endDate: string,
+   }
+}
+export type Entry =
+   | HospitalEntry
+   | OccupationalHealthcareEntry
+   | HealthCheckEntry;
 
 export interface Patient {
    id: string,
@@ -21,5 +59,6 @@ export interface Patient {
    occupation: string
    entries: Entry[]
 }
+
 export type NewPatient = Omit<Patient, 'id'>
 export type NonSensitivePatientData = Omit<Patient, 'ssn' | 'entries'>
