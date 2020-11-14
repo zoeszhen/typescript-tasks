b3edbdbc2296906a54d95af9d8e4a7f485988aff
SHI ZHEN <shi.zhen@vincit.fi>
2020-11-14 15:29:30 +0200

    feat(9.9): Fix cors problem

--- patientor/index.ts
+++ patientor/index.ts
@@ -1,12 +1,18 @@
 import express from 'express';
+const cors = require('cors')
+
 const app = express();
+app.use(cors())
 app.use(express.json())
 
-app.get('/ping', (_req, res) => {
+/** For rest client
+  GET http://localhost:3001/api/ping
+*/
+app.get('/api/ping', (_req, res) => {
   res.send('pong');
 });
 
-const PORT = 3003;
+const PORT = 3001;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
